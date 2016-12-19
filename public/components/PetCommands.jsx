25d08fa9df4ae3e7a3fa51922e12f53c41ed9684
Jarob Gilliam <jarobgilliambiz@gmail.com>
2016-12-18 18:09:02 -0800

    Fix game icon size

--- public/components/PetCommands.jsx
+++ public/components/PetCommands.jsx
@@ -1,11 +1,11 @@
-avar PetCommand = (props) => (
+var PetCommand = (props) => (
   <div className="pet-commandbar">
     <img className='command' src={props.cmdImg.food} onClick={() => {props.executeCommand('eating')}}/>
     <img className='command' src={props.cmdImg.love} onClick={() => {props.executeCommand('playing')}}/>
     <img className='command' src={props.cmdImg.code} onClick={() => {props.executeCommand('coding')}}/>
     <img className='command' src={props.cmdImg.sleep} onClick={() => {props.executeCommand('sleeping')}}/>
     <img className='command' src={props.cmdImg.question} onClick={() => {props.executeCommand('question')}}/>
-    <a href='/game'><img className='command' src='../other/img/gude.png' /></a>
+    <a href='/game'><img className='command' src='../other/img/gude_big.png' /></a>
 
   </div>
 );
